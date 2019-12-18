7071eeff0a384a9eb9a03a602455ac45a3e1fa82
halmulaiki <56926472+halmulaiki@users.noreply.github.com>
2019-12-17 23:26:56 -0500

    got the api working gave prices and needs more styling

--- bakery.rb
+++ bakery.rb
@@ -1,34 +1,49 @@
 require 'sinatra'
 require 'httparty'
 
-class Cookies
-attr_accessor :description, :price, :name ,:img
+class Cookie
+attr_accessor :ingredientLines, :price, :name ,:img
 
-   def initialize(name,img, description, price)
+   def initialize(id, name,img, ingredientLines, price)
        @name = name
-       @price = price
-       @description = description
+       @price = 29.99
+       @ingredientLines = ingredientLines
        @img = img
 
    end
 
 end
 class Cake
-    def initialize(name,img, description, price)
+attr_accessor :ingredientLines, :price, :name ,:img
+
+    def initialize(id, name,img, ingredientLines, price)
         @name = name
-        @price = price
-        @description = description
+        @price = 59.99
+        @ingredientLines = ingredientLines
         @img = img
- 
     
     end
 end
 class Muffin
-    def initialize(name,img, description, price)
+ attr_accessor :ingredientLines, :price, :name ,:img
+
+    def initialize(id, name,img, ingredientLines, price)
         @name = name
-        @price = price
-        @description = description
+        @price = 41.99
+        @ingredientLines = ingredientLines
         @img = img
  
     end
 end
+class Bread
+    attr_accessor :ingredientLines, :price, :name ,:img
+   
+       def initialize(id, name,img, ingredientLines, price)
+           @name = name
+           @price = 20.99
+           @ingredientLines = ingredientLines
+           @img = img
+    
+       end
+   end
+# @cake = Cake.new("Butter Cake",https://assets.epicurious.com/photos/575991f3973781fc02c2a827/6:4/w_1600%2Cc_limit/EP_06062016_Vanilla-Buttermilk-Wedding-Cake-with-Raspberries-and-Orange-Cream-Cheese-Frosting.jpg, 'Any recipe for cake that begins "cream butter and sugar" is a butter cake. After the creaming, you add eggs to aerate the batter a bit, flour (and sometimes another liquid, like milk) to give it structure and texture, and baking powder or baking soda to ensure that it rises in the oven. Different types of cake batter within the butter cake family include chocolate, white, yellow and marble; for white and yellow cakes coloring typically depends on whether they have whole eggs, or extra egg yolks in them (yellow cake) or egg whites only (white cake).', "$30.00")
--- server.rb
+++ server.rb
@@ -1,25 +1,116 @@
 require 'sinatra'
 require 'httparty'
+require 'curb'
+require 'json'
+require "./bakery.rb"
 set :port, 3000
 
-@@url = 'https://samples.openweathermap.org/data/2.5/weather'
-
+# @@url = "https://api.spoonacular.com/recipes/random?number=10&tags=vegetarian,dessert&apiKey=d949234bbafe45df8068c75f1821d320"
+@@path = "https://api.edamam.com/search"
 get '/' do
-
+#{ENV['MY_API']}
   erb :index
 end
+get '/bread' do
 
+
+ 
+@food ="bread"
+  @titlePage = "bread"
+  @number = 22
+  @id = ENV['MY_APP']
+  @key = ENV['MY_API_KEY']
+  @arr = []
+  req = Curl::Easy.perform(@@path + "?q=#{@food}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@number}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+  i = 10
+  while i < @number
+    @data = res['hits'][i]["recipe"]
+    @ingredientLines = []
+    @data["ingredientLines"].each do |y|
+      @ingredientLines << y
+    end
+    @bread = Bread.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
+    @arr << @bread
+    i += 1
+  end
+
+
+
+   puts res
+  erb :bread
+end
 get '/cookies' do
 
-  erb :cookies
+    @food ="cookie"
+    @titlePage = "cookie"
+    @number = 22
+    @id = ENV['MY_APP']
+    @key = ENV['MY_API_KEY']
+    @arr = []
+    req = Curl::Easy.perform(@@path + "?q=#{@food}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@number}&calories=591-722&health=alcohol-free")
+    res = JSON.parse(req.body)
+    i = 10
+    while i < @number
+      @data = res['hits'][i]["recipe"]
+      @ingredientLines = []
+      @data["ingredientLines"].each do |description|
+        @ingredientLines << description
+      end
+      @cookie = Cookie.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
+      @arr << @cookie
+      i += 1
+    end
+    puts res
+    erb :cookies
+
 end
 
 get '/cake' do
-
+  @food ="cake"
+  @titlePage = "cake"
+  @number = 22
+  @id = ENV['MY_APP']
+  @key = ENV['MY_API_KEY']
+  @arr = []
+  req = Curl::Easy.perform(@@path + "?q=#{@food}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@number}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+  i = 10
+  while i < @number
+    @data = res['hits'][i]["recipe"]
+    @ingredientLines = []
+    @data["ingredientLines"].each do |description|
+      @ingredientLines << description
+    end
+    @cake = Cake.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
+    @arr << @cake
+    i += 1
+  end
+  puts res
   erb :cake
 end
 
 get '/muffin' do
+  @food ="muffin"
+  @titlePage = "muffin"
+  @number = 22
+  @id = ENV['MY_APP']
+  @key = ENV['MY_API_KEY']
+  @arr = []
+  req = Curl::Easy.perform(@@path + "?q=#{@food}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@number}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+  i = 10
+  while i < @number
+    @data = res['hits'][i]["recipe"]
+    @ingredientLines = []
+    @data["ingredientLines"].each do |description|
+      @ingredientLines << description
+    end
 
+    @muffin = Muffin.new(i,@data["label"], @data["image"],@ingredientLines, 2)
+    @arr << @muffin
+    i += 1
+  end
+  puts res
   erb :muffin
 end
